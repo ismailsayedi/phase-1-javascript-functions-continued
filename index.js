b124fd5dd799014acc9ec04d80751f6348a53ac2
ismail <ismail.syedy@gmail.com>
2023-03-30 20:12:37 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = 'go to the office') {
+    const todo = activity ? `This Monday, I will ${activity}.` : `This Monday, I will go to the office.`;
+    return todo;
+  }
+  
+  function wrapAdjective(flair = '*') {
+    return function(value= 'special') {
+      return `You are ${flair}${value}${flair}!`;
+    }
+  }
+  const encouragingPromptFunction = wrapAdjective("!!!")
+  wrapAdjective("%")("a dedicated programmer");
+  
